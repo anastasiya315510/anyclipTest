8bae11b8d377f9bbec0dfdd4e4d4f6972fc108c3
anastasiya315510 <315510a@gmail.com>
2021-08-06 20:09:14 +0300

    Initial commit

---
+++ src/main/java/com/example/anycliptest/AnyclipTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.anycliptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyclipTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnyclipTestApplication.class, args);
+    }
+
+}
